9486c3d8932e211c4127f897267a9a2fb2ecb709
Terry.Tu <tuminfei1981@gmail.com>
2023-11-22 10:12:12 +0800

    update wasm

---
+++ README.md
@@ -0,0 +1,7 @@
+# icrc7_with_assets
+
+## note
+- This project was written taking reference of [icrc7](https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-7/ICRC-7.md)
+
+## assets
+
---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!!/bin/bash
+
+# create candid file
+cargo test
+
+# gzip wasm
+
+# deploy
+
+dfx deploy icrc7_with_assets --argument '(record {
+  tx_window=24;
+  permitted_drift=2;
+  name="Space";
+  symbol="Space";
+  minting_authority=opt principal"3yyxm-t5fpe-v32em-ac6lr-xyort-wuscb-dvl4x-3wnwi-hqkyj-xortw-oqe";
+  royalties=null;
+  royalties_recipient=null;    
+  description=opt "ICRC7 Standard Token";
+  image=null;    
+  supply_cap=null;    
+})'
+
+# mint
+
+dfx canister call icrc7_with_assets icrc7_mint '(record{
+  id=100;
+  name="Icrc7 100";
+  description=opt "100th token of the collection";
+  image=null;
+  to=record{
+  owner=principal"3yyxm-t5fpe-v32em-ac6lr-xyort-wuscb-dvl4x-3wnwi-hqkyj-xortw-oqe";
+  subaccount=null;
+  };
+})'
+
+
+
